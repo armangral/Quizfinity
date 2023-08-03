ebf1014b867254e5cef7da46e336d6d8019f8cb8
abdul rehman <abdulrehmanmangral86@gmail.com>
2023-08-04 01:44:33 +0500

    handling answers

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,9 @@ const initialState = {
   questions: [],
   // "loading","error","ready","active","finished"
   status: "loading",
+  index: 0,
+  answer: null,
+  points: 0,
 };
 
 function reducer(state, action) {
@@ -31,19 +34,34 @@ function reducer(state, action) {
         ...state,
         status: "active",
       };
+
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
     default:
       throw new Error("Action unknown");
   }
 }
 
 export default function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   const numQuestions = questions.length;
 
   useEffect(function () {
     fetch(
-      "https://raw.githubusercontent.com/armangral/Quizfinity/master/data/questions.json"
+      // "https://raw.githubusercontent.com/armangral/Quizfinity/master/data/questions.json"
+      "http://localhost:8000/questions"
     )
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataReceived", payload: data }))
@@ -59,7 +77,13 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question />}
+        {status === "active" && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
       </Main>
     </div>
   );
