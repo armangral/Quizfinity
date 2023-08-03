b24545975ad3ea02c24dcf1c245323cc02727f96
abdul rehman <abdulrehmanmangral86@gmail.com>
2023-08-03 17:24:12 +0500

    changing start screen

--- src/StartScreen.js
+++ src/StartScreen.js
@@ -1,9 +1,9 @@
-function StartScreen() {
+function StartScreen({ numQuestions }) {
   return (
     <div className="start">
       <h2>Welcome to Quizfinity</h2>
-      <h3>X questions to test your React mastery</h3>
-      <button>Let's Start</button>
+      <h3>{numQuestions} questions to test your React mastery</h3>
+      <button className="btn btn-ui">Let's Start</button>
     </div>
   );
 }
