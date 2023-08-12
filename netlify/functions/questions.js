a6228fc756b7c5efbb7d29043afd84b40ed986c0
armangral <91218206+armangral@users.noreply.github.com>
2023-08-12 22:29:11 +0500

    adding more questions

--- netlify/functions/questions.js
+++ netlify/functions/questions.js
@@ -1,9 +1,20 @@
+const questionsData = require("./questions.json");
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
 exports.handler = async function (event, context) {
   try {
-    const questions = require("./questions.json");
+    const allQuestions = questionsData.questions;
+    const shuffledQuestions = shuffleArray(allQuestions);
     return {
       statusCode: 200,
-      body: JSON.stringify(questions),
+      body: JSON.stringify({ questions: shuffledQuestions.slice(0, 10) }),
     };
   } catch (error) {
     return {
